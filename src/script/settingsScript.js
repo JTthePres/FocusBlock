b8908059a67abb1bc4af10d902fc3a529d130c38
JTthePres <jacopo.tamarri@outlook.it>
2023-12-28 17:45:07 +0100

    fixed updateUi at the delete

--- src/script/settingsScript.js
+++ src/script/settingsScript.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateUI(urlList) {
     // Crea un elenco non ordinato (ul) per visualizzare gli URL
+    this.document.querySelector('#list').innerHTML = '';
     var element_index = 1;
     // Popola l'elenco con gli URL dallo storage
     urlList.forEach((url) => {
@@ -25,96 +26,82 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Itera su ciascun pulsante e aggiungi un gestore di eventi
     for (var i = 0; i < deleteButtons.length; i++) {
-      deleteButtons[i].addEventListener("click", function () {
-        var key = this.parentNode.children[0].getAttribute("data-value");
-
-        console.log(key);
-        typeof key;
-        var index;
-        chrome.storage.sync.get({ urlList: [] }, (data) => {
-          // Recupera l'array urlList dallo storage o inizializzalo come un array vuoto se non esiste
-          let urlList = data.urlList;
-          remove_index = urlList.indexOf(key);
-          let tmp = urlList.splice(remove_index, 1);
-          // Salva l'array aggiornato nello storage di sincronizzazione
-          chrome.storage.sync.set({ urlList: urlList }, () => {
-            console.log("URL eliminato correttamente.");
-            updateUI(urlList);
-          });
-          tmp = remove_index + 1;
-          console.log(remove_index);
-          chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds: [tmp]
-          }, function () {
-            if (chrome.runtime.lastError) {
-              console.error("Errore durante l'eliminazione delle regole di reindirizzamento:", chrome.runtime.lastError);
-            } else {
-              console.log("Regola eliminata con successo!");
-            }
-          });
-          for (let j = tmp; j < element_index; j++) {
-              chrome.declarativeNetRequest.updateDynamicRules({
-                removeRuleIds: [j]
-              }, function () {
-                if (chrome.runtime.lastError) {
-                  console.error("Errore durante l'eliminazione delle regole di reindirizzamento:", chrome.runtime.lastError);
-                } else {
-                  console.log("Regola eliminata con successo!",j);
-                }
-              });
-            }
-          urlList.forEach((url, index) => {
-            url+= "*"
-            if (index >= remove_index){
-            chrome.declarativeNetRequest.updateDynamicRules({
-              addRules: [
-                {
-                  id: index+1,
-                  priority: 1,
-                  action: { type: 'redirect',       
-                   "redirect": {
-                    "extensionPath": "/src/pages/splashPage.html"   
-                  } },
-                  condition: { urlFilter: url, resourceTypes: ['main_frame'] },
-                },
-              ],
-            });}
-          });
-
-
-          
-        });
+      deleteButtons[i].addEventListener("click", function(event) {
+        manageDelete(event, element_index);
       });
     }
-  }
-});
+          }
+          
+
+          
+        function manageDelete(event,element_index) {
+          var key = event.currentTarget.parentNode.children[0].getAttribute("data-value");
+          console.log(key);
+          typeof key;
+          chrome.storage.sync.get({ urlList: [] }, (data) => {
+            let urlList = data.urlList;
+            remove_index = urlList.indexOf(key);
+            let tmp = urlList.splice(remove_index, 1);
+
+            chrome.storage.sync.set({ urlList: urlList }, () => {
+              console.log("URL eliminato correttamente.");
+              updateUI(urlList);
+            });
+
+    tmp = remove_index + 1;
+    console.log(remove_index);
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [tmp]
+    }, function () {
+      if (chrome.runtime.lastError) {
+        console.error("Errore durante l'eliminazione delle regole di reindirizzamento:", chrome.runtime.lastError);
+      } else {
+        console.log("Regola eliminata con successo!");
+      }
+    });
+
+    for (let j = tmp; j < element_index; j++) {
+      chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [j]
+      }, function () {
+        if (chrome.runtime.lastError) {
+          console.error("Errore durante l'eliminazione delle regole di reindirizzamento:", chrome.runtime.lastError);
+        } else {
+          console.log("Regola eliminata con successo!", j);
+        }
+      });
+    }
+
+    urlList.forEach((url, index) => {
+      url += "*";
+      if (index >= remove_index) {
+        chrome.declarativeNetRequest.updateDynamicRules({
+          addRules: [
+            {
+              id: index + 1,
+              priority: 1,
+              action: {
+                type: 'redirect',
+                "redirect": {
+                  "extensionPath": "/src/pages/splashPage.html"
+                }
+              },
+              condition: { urlFilter: url, resourceTypes: ['main_frame'] },
+            },
+          ],
+        });
+      }
+    });
+  });
+} // Move this closing bracket to the correct position
 
 function deleteAll() {
   cleanNetBlockList();
   chrome.storage.sync.set({ urlList: [] }, () => {
     console.log("Lista pulita correttamente.");
-    updateUI(urlList);
-  });
-}
+  });    
+  document.querySelector('#list').innerHTML = '';
 
-function cleanNetBlockList() {
-  chrome.storage.sync.get({ urlList: [] }, (data) => {
-    // Recupera l'array urlList dallo storage o inizializzalo come un array vuoto se non esiste
-    let urlList = data.urlList;
-    urlList.forEach((url, index) => {
-      deleteRuleNetBlockList(index + 1);
-    });   
-  });
 }
-
-function deleteRuleNetBlockList(index) {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [index]
-  }, function() {
-    if (chrome.runtime.lastError) {
-      console.error("Errore durante l'eliminazione delle regole di reindirizzamento:", chrome.runtime.lastError);
-    } else {
-      console.log("Regola eliminata con successo!");
-    }
-  }); 
-}
+});
--- src/script/toggleScript.js
+++ src/script/toggleScript.js
@@ -24,34 +24,3 @@ toggle.addEventListener('change', function() {
   }
 });
 
-function createNetBlockList() {
-  chrome.storage.sync.get({ urlList: [] }, (data) => {
-    // Recupera l'array urlList dallo storage o inizializzalo come un array vuoto se non esiste
-    let urlList = data.urlList;
-    urlList.forEach((url, index) => {
-      addNetBlockList(url, index + 1);
-    });   
-
-  });
-}
-function cleanNetBlockList() {
-  chrome.storage.sync.get({ urlList: [] }, (data) => {
-    // Recupera l'array urlList dallo storage o inizializzalo come un array vuoto se non esiste
-    let urlList = data.urlList;
-    urlList.forEach((url, index) => {
-      deleteRuleNetBlockList(index + 1);
-    });   
-  });
-}
-
-function deleteRuleNetBlockList(index) {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [index]
-  }, function() {
-    if (chrome.runtime.lastError) {
-      console.error("Errore durante l'eliminazione delle regole di reindirizzamento:", chrome.runtime.lastError);
-    } else {
-      console.log("Regola eliminata con successo!");
-    }
-  }); 
-}
